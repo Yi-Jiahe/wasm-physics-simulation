4d5793e0578c6022f7e692eb702f99c15875deef
yijiahe <yi.jiahe00@gmail.com>
2022-07-13 08:30:08 +0000

    Try out new, add and update methods from rust-physics-engine 0.1.2

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-import * as wasm from "hello-wasm-pack";
+import { World2D, Body2D } from "rust-physics-engine";
 
-wasm.greet();
+const world = World2D.new();
+
+console.log(world);
+
+world.add(Body2D.new(1.0, [0.0, 0.0]));
+
+console.log(world);
+
+const renderLoop = () => {
+    world.update();
+    console.log(world);
+
+    requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
